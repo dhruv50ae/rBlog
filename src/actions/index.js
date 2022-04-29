059923368739c85565237440743ed56964a8fc3b
dhruv50ae <linkdp2012@gmail.com>
2022-04-29 15:03:37 +0100

    Refactored the memoize function

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,9 @@
+import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPostsAndUsers = () => async (dispatch) => {
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
   await dispatch(fetchPosts());
+  const userIds = _.uniq(_.map(getState().posts, "userId"));
 };
 
 export const fetchPosts = () => async (dispatch) => {
