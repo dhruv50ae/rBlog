e8d09eb96a913cf42f2920d52ab91992b407e390
dhruv50ae <linkdp2012@gmail.com>
2022-04-29 13:05:31 +0100

    Make HTTPS request to website

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -7,7 +7,11 @@ class PostList extends Component {
     this.props.fetchPosts();
   }
   render() {
+    console.log(this.props.posts);
     return <div>PostList</div>;
   }
 }
-export default connect(null, { fetchPosts })(PostList);
+const mapStateToProps = (state) => {
+  return { posts: state.posts };
+};
+export default connect(mapStateToProps, { fetchPosts })(PostList);
